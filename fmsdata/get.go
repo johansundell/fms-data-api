254018354b18a6a0f695fe295293bde9e546d490
Johan Sundell <johansundell@gmail.com>
2020-11-19 15:12:21 +0100

    Added a max to get all :)

--- fmsdata/get.go
+++ fmsdata/get.go
@@ -8,3 +8,8 @@ func (d *DataBase) GetAllFrom(layout string) ([]byte, error) {
 	url := fmt.Sprintf("%s/fmi/data/v1/databases/%s/layouts/%s/records?_limit=20000", d.url, d.database, layout)
 	return d.makeCall(url, "GET", nil)
 }
+
+func (d *DataBase) GetAllFromWithMax(layout string, max int) ([]byte, error) {
+	url := fmt.Sprintf("%s/fmi/data/v1/databases/%s/layouts/%s/records?_limit=%d", d.url, d.database, layout, max)
+	return d.makeCall(url, "GET", nil)
+}
