315c9a38fad5f51a5aac907e08dbf5fb243a333d
Johan Sundell <johansundell@gmail.com>
2022-06-09 12:44:14 +0200

    Update post.go

--- fmsdata/post.go
+++ fmsdata/post.go
@@ -8,3 +8,8 @@ func (d *DataBase) Post(layout string, data interface{}) ([]byte, error) {
 	url := fmt.Sprintf("%s/fmi/data/v1/databases/%s/layouts/%s/records", d.url, d.database, layout)
 	return d.makeCall(url, "POST", data)
 }
+
+func (d *DataBase) PostWithScript(layout string, data interface{}, script string) ([]byte, error) {
+	url := fmt.Sprintf("%s/fmi/data/v1/databases/%s/layouts/%s/records?script=%s", d.url, d.database, layout, script)
+	return d.makeCall(url, "POST", data)
+}
